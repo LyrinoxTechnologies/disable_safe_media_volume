5cd5c51c0d7ee66827409045d51b0ddaa9902b65
Lyrinox Technologies <contact@lyrinox.com>
2026-03-14 09:08:32 -0600

    Add media volume settings and CSD enforcement
    Added legacy media volume settings and CSD enforcement for EU Android 14+.

--- service.sh
+++ service.sh
@@ -4,11 +4,16 @@ exec 2>$MODPATH/log.txt
 set -x
 
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
-	sleep 1
+    sleep 1
 done
 
+# Legacy safe media volume settings
 settings put global audio_safe_volume_state 0
 settings put global safe_headset_volume 0
 settings put system volume_link_notification 1
 settings delete global audio_safe_media_volume_index
 settings put system safe_headset_volume_index 100
+
+# CSD (Content Sound Dosimetry) - EU Android 14+ enforcement
+settings put global audio_safe_csd_enabled 0
+settings put global audio_safe_csd_dose 0
